32b7060ea3b1b4d6bd31f53b412c5364cc8b4118
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2024-07-15 12:48:30 +0200

    Finish 04_removeFromArray exercise
    Write a function that takes an array and some other arguments then
    removes the other arguments from that array.
    
    Test it with the removeFromArray.spec.js file, first with the first
    test of the file and then enabling the other 7 tests by removing the
    ".skip" from the "test.skip()" functions.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,28 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...elementsToRemove) {
+
+  return array.filter(val => !elementsToRemove.includes(val))
+
+}
+
+/*  My first solution:
+
+  const sortedArray = array.sort();
+  const sortedElementsToRemove = elementsToRemove.sort();
+  let newArray;
+
+  if (sortedArray.toString() === sortedElementsToRemove.toString()) {
+    newArray = [];
+  } else {
+    newArray = array.filter(
+      element => 
+        (element !== elementsToRemove[0]) && 
+        (element !== elementsToRemove[1])
+    )
+  }
+
+  return newArray;
+
+*/
 
 // Do not edit below this line
 module.exports = removeFromArray;
