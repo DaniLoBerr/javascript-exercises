04b64675cff654536812c7abf07e79adc47bd287
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2024-07-17 19:44:07 +0200

    Finish 07_tempConversion exercise
    Write two functions that convert temperatures from Fahrenheit to
    Celsius, and vice versa, rounded to one decimal place.
    
    Test it with the tempConversion.spec.js file, first with the first
    tests of the file of each function and then enabling the other tests
    functions by removing the ".skip" from the "test.skip()" functions.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+
+  const fahrenheitToCelsiusFormula = ((temperatureInFahrenheit - 32) * (5/9));
+  const conversionRoundedToOneDecimal = +fahrenheitToCelsiusFormula.toFixed(1);
+  return conversionRoundedToOneDecimal;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+
+  const celsiusToFahrenheitFormula = (temperatureInCelsius * (9/5) + 32);
+  const conversionRoundedToOneDecimal = +celsiusToFahrenheitFormula.toFixed(1);
+  return conversionRoundedToOneDecimal;
+
 };
 
 // Do not edit below this line
