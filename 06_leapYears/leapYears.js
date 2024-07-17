e165378bb0cb2823180ff325df4278a19d9e826e
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2024-07-17 19:03:50 +0200

    Finish 06_leapYears exercise
    Write a function that determines whether or not a given year is
    a leap year.
    
    Test it with the leapYears.spec.js file, first with the first
    test of the file and then enabling the other 5 tests by removing the
    ".skip" from the "test.skip()" functions.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,23 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+  if(
+    (year % 4 === 0) &&
+    (
+      (year % 400 === 0) || 
+      (year % 100 !== 0)
+    ) 
+  ) return true;
+  else return false;
+
+  /* 
+    CONSEJO PARA REFACTORIZAR
+  
+    Para que el código sea más comprensible, es bueno no utilizar
+    números en bruto y seria más recomendable almacenar las operaciones
+    en variables bien explicativas y luego trabajar con esas variables
+    (tal cual hacen en la solución).
+  
+  */
 };
 
 // Do not edit below this line
