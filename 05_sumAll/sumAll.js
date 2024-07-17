ca8cf7194872327e5b05d226f7eb52b3e6180f73
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2024-07-17 18:47:27 +0200

    Finish 05_sumAll exercise
    Write a function that takes 2 positive integers and returns the sum of
    every integer between (and including) them:
    
    Test it with the sumAll.spec.js file, first with the first
    test of the file and then enabling the other 5 tests by removing the
    ".skip" from the "test.skip()" functions.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,39 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+  let result =  0;
+  const sortedArrNums = [num1, num2].sort();
+
+  if (
+    (num1 < 0 || 
+    num2 < 0) ||
+    (!Number.isInteger(num1) || 
+    !Number.isInteger(num2))
+  ) result = "ERROR";
+  else {
+    for (let i = sortedArrNums[0]; i <= sortedArrNums[1]; i++) {
+      result += i;
+    }
+  }
+
+  /* 
+  
+  Formas alternativas ordenar el valor mínimo y el valor máximo del rango:
+
+  #1
+
+    if (min > max) {
+      const temp = min;
+      min = max;
+      max = temp;
+    }
+
+  #2 Utilizando una desestructuración de arrays:
+
+    if (min > max) [min, max] = [max, min];
+
+  */
+  
+  return result;
 };
 
 // Do not edit below this line
