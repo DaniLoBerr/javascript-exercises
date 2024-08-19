03d7781762097ac4a0113f2470252c58f8f557a5
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2024-08-19 11:13:35 +0200

    Complete "09_palindromes" exercise
    Write a function that determines whether or not a given string is a
    palindrome.
    
    Test it with the "palindromes.spec.js" file.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const cleanedString = string
+    .toLowerCase()
+    .replace(/[,.!? ]/g, "");
+  const reversedCleanedSring = cleanedString
+    .split("")
+    .toReversed()
+    .join("")
+  return cleanedString === reversedCleanedSring;
 };
 
 // Do not edit below this line
