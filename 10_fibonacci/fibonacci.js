db7962b34744b81767ca711b905985e894bf7056
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2024-08-19 15:31:22 +0200

    Complete "10_fibonacci" exercise
    Create a function that returns a specific member of the Fibonacci sequence.
    
    Test it with the "fibonacci.spec.js" file.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+  const numericMember = +member;
+  if (numericMember < 0 || isNaN(numericMember)) return "OOPS";
+  
+  let fibonacciArr = [0, 1]
+  for (let i = 2; i <= numericMember; i++) {
+    fibonacciArr.push(fibonacciArr[i-1] + fibonacciArr[i-2]);
+  }
+  return fibonacciArr[numericMember];
 };
 
 // Do not edit below this line
