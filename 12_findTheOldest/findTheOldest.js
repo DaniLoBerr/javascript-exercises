ebf5d1a1c7e03f78dce132015b13dc226178b742
Daniel López Berrocal <97476157+DaniLoBerr@users.noreply.github.com>
2024-08-19 18:25:42 +0200

    Complete "12_findTheOldest" exercise
    Create a function that takes an array of objects representing people with a birth and death year and returns the oldest person.
    
    Test it with the "findTheOldest.spec.js" file.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  const oldestToYoungestAgesArr = people
+    .map(person => {
+      const currentYear = new Date().getFullYear();
+      if (person.yearOfDeath === undefined) person.yearOfDeath = currentYear;
+      return person.yearOfDeath - person.yearOfBirth;
+    })
+    .sort((person1, person2) => person2 - person1);
 
+  const oldestAge = oldestToYoungestAgesArr[0];
+
+  return people.find(person => (
+    person.yearOfDeath - person.yearOfBirth === oldestAge
+  ));
 };
 
 // Do not edit below this line
